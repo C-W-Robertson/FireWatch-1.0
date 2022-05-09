6b7a4ea3c57d4452e99a9ec26028918fffaedd68
CW Robertson <84760399+C-W-Robertson@users.noreply.github.com>
2022-05-09 11:08:46 -0700

    Update index.js
    Added additional comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// index.js imports the .App wrapper, CSS, and ReactDOM //
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
